4c9cb6e036c66961a25987ef2da9ec4dd779be06
AnjuTewatia <anjutewatia008@gmail.com>
2023-01-19 12:14:39 +0530

    footer

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import AllRoutes from "./All_Routes/AllRoutes"
+
+function App() {
+  return (
+    <div className="App">
+      <AllRoutes/>
+    </div>
+  );
+}
+
+export default App;
