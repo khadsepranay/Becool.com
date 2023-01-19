0ff2626d856dde05e95534803b4f48c0405556e0
coderV1 <pawarvikas534@gmail.com>
2023-01-19 14:06:15 +0530

    restore the main file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,6 @@
-
-import AllRoutes from "./All_Routes/AllRoutes";
+import logo from './logo.svg';
 import './App.css';
-
+import AllRoutes from "./All_Routes/AllRoutes"
 
 function App() {
   return (
