732b8bdcaad4f2255f3b03039ce52739404b2dab
Pranay Khadse <pranitkhadse20@gmail.com>
2023-01-19 11:41:49 +0530

    changes

--- frontend/src/Redux/Store.js
+++ frontend/src/Redux/Store.js
@@ -1,8 +1,8 @@
 let { legacy_createStore, applyMiddleware, combineReducers } = require("redux");
 let thunk = require("redux-thunk");
 
-let rootReducer = combineReducers({});
+// let rootReducer = combineReducers({});
 
-let store = legacy_createStore(rootReducer, applyMiddleware(thunk));
+// let store = legacy_createStore(rootReducer, applyMiddleware(thunk));
 
 export default store;
