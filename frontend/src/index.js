64c36484636724dca043ba91053c71b81427c353
Pranay Khadse <pranitkhadse20@gmail.com>
2023-01-19 11:59:18 +0530

    changes

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,15 +5,13 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from "react-router-dom";
 import { Provider } from 'react-redux';
-import store from './Redux/Store';
+// import store from './Redux/Store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Provider store={store}>
   <BrowserRouter>
     <App />
   </BrowserRouter>
-  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
